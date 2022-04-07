03196f27c29bfbc89c4b3ad5590b961c1db85392
Brendan Jurd <brendan.jurd@geoplex.com.au>
2022-04-07 15:24:30 +1000

    Update run_test_on_update.sh to work with fswatch.
    fswatch is a cross-platform utility that provides a similar
    functionality to inotifywait on linux.  The script now tests to see if
    you've got fswatch installed, and uses it if so.  Otherwise it tests to
    see if you've got inotifywait, and uses that if so.  If neither are
    found, the script errors out.

--- run_test_on_update.sh
+++ run_test_on_update.sh
@@ -1,7 +1,25 @@
-inotifywait -m -e create . | while read -r dir event filename; do
-    if [[ "$filename" == *.py ]]; then
+#!/bin/bash
+#
+# Monitor for changes to Python files in the current directory, and when a
+# change is detected, run the test suite.
+#
+# Requires that the user have either `fswatch` or `inotifywait` installed and
+# in their PATH.
+if command -v fswatch > /dev/null 2>&1; then
+    fswatch -0 -l 0.2 --event Created --include '\.py$' --exclude '.*' . | while read -r -d '' filename; do
         echo "Change detected in $filename, running tests ..."
         python3 -m unittest
         echo
-    fi
-done
+    done
+elif command -v inotifywait > /dev/null 2>&1; then
+    inotifywait -m -e create . | while read -r dir event filename; do
+        if [[ "$filename" == *.py ]]; then
+            echo "Change detected in $filename, running tests ..."
+            python3 -m unittest
+            echo
+        fi
+    done
+else
+    echo "ERROR: Neither fswatch nor inotifywait found, quitting."
+    exit 1
+fi
