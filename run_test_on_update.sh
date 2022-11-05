5b6becf4bbcf51c130a13bc26425bd7f14cce3be
Brendan Jurd <direvus@gmail.com>
2022-11-05 11:28:52 +1100

    Run tests once initially in run_test_on_update.sh.

--- run_test_on_update.sh
+++ run_test_on_update.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 #
-# Monitor for changes to Python files in the current directory, and when a
-# change is detected, run the test suite.
+# Run the test suite, then monitor for changes to Python files in the current
+# directory, and each time a change is detected, run the test suite again.
 #
 # Requires that the user have either `fswatch` or `inotifywait` installed and
 # in their PATH.
+
+python3 -m unittest
+
 if command -v fswatch > /dev/null 2>&1; then
     fswatch -0 -l 0.2 --event Created --include '\.py$' --exclude '.*' . | while read -r -d '' filename; do
         echo "Change detected in $filename, running tests ..."
